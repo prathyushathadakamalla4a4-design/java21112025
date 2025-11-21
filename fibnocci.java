77fa8d9fa6730cb13c4a2afd97bf6a0c0b1912b7
Thadakamalla prathyusha <prathyushathadakamalla4a4@gmail.com>
2025-11-21 13:09:16 +0530

    push to another

---
+++ fibnocci.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practice03112025;
+
+/**
+ *
+ * @author THADAKAMALLA
+ */
+ abstract class fb
+{
+    int f;
+    int n1;
+    int n2;
+
+    }
+class fb2 extends fb
+{
+    int sum;
+    public fb2(int sum)
+    {
+       
+       this.sum = sum;
+        // super(f, n1, n2);
+    }
+    public void displayinformation(){
+        System.out.println("fibnocci series");
+        for(int i =0 ; i<= sum ; i++){
+            
+        }
+    }
+}
+public class fibnocci {
+    public static void main(String[] args) {
+        
+    }
+}
